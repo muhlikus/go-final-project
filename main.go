f40105810395867f1b12af4c666609ba2ae3a963
Danilenko Mike <mike@danilenko.name>
2024-12-01 11:52:11 +0100

    first stage, extra task

--- main.go
+++ main.go
@@ -1,17 +1,27 @@
 package main
 
-import "net/http"
-
-const (
-	serverPort = ":7540"
-	webDir     = "./web/"
+import (
+	"fmt"
+	"net/http"
+	"os"
 )
 
+const webDir = "./web/"
+
+var serverPort string = "7540"
+
 func main() {
+
+	// check if "TODO_PORT" set
+	if port, ok := os.LookupEnv("TODO_PORT"); ok {
+		//log.Printf("set listening port from ENV to: %s", port)
+		serverPort = port
+	}
+
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(webDir)))
 
-	err := http.ListenAndServe(serverPort, mux)
+	err := http.ListenAndServe(fmt.Sprintf(":%s", serverPort), mux)
 	if err != nil {
 		panic(err)
 	}
